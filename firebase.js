26cdaf53dc37aacb5f6f27de785a818a76e05a4c
tiaewneehur <ice.iceza2014@gmail.com>
2026-08-04 12:51:02 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,1 @@
 
-const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "tiaewneehur.firebaseapp.com",
-  projectId: "tiaewneehur",
-  storageBucket: "tiaewneehur.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-const auth = firebase.auth();
-const db = firebase.firestore();
